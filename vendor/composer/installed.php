765352f5f30718c1590eeb3f2e055e61addb0ec8
Jerome Barbato <jerome.barbato@gmail.com>
2024-01-23 10:33:44 +0100

    allow loading using bedrock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iamntz/carbon-fields-urlpicker',
+        'pretty_version' => '3.0.10',
+        'version' => '3.0.10.0',
+        'reference' => '0d5014d078dc797af33829c9ad1aee2ae4074fd4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iamntz/carbon-fields-urlpicker' => array(
+            'pretty_version' => '3.0.10',
+            'version' => '3.0.10.0',
+            'reference' => '0d5014d078dc797af33829c9ad1aee2ae4074fd4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
